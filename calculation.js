8b0a35e433519cd5a28855e90b0fdf01b5a3b937
Andrii Zadorozhnii <zador746@gmail.com>
2023-03-03 19:14:02 +0200

    m

--- calculation.js
+++ calculation.js
@@ -1,35 +1,23 @@
 let inputArgumentsList = [];
 
-let addNumbersToListButton = document.querySelector(".excersize-two__button-add");
-let calculateItemsButton = document.querySelector(".excersize-two__button-calculate");
+let addNumbersToListButton = document.querySelector(
+    ".excersize-two__button-add"
+);
+let calculateItemsButton = document.querySelector(
+    ".excersize-two__button-calculate"
+);
 let inputArgument = document.querySelector(".excersize-two__argument");
 
 
 
-// 3
+// _____3______
 
+let excersThreeButton = document.querySelector(".excersize-three__button");
 let firstNumber = document.querySelector(".excersize-three__input-one");
 let secondNumber = document.querySelector(".excersize-three__input-two");
 let excersThree = document.querySelector(".excersize-three__unswer");
-let excersThreeButton = document.querySelector(".excersize-three__button");
 
-// excersThreeButton.onClick = function excersizeThree(firstNumber, secondNumber) {
-//     if (+firstNumber.value < +seconNumber.value) {
-//         excersThree.innerHTML = "-1";
-
-//         return excersThree;
-//     } else if (+firstNumber.value > +secondNumber.value) {
-//         excersThree.innerHTML = "1";
-
-//         return excersThree;
-//     } else if (+firstNumber.value === +secondNumber.value) {
-//         excersThree.innerHTML = "0";
-//         console.log("0");
-//     }
-// }
-
-
-function excersizeThree(firstNumber, secondNumber) {
+excersThreeButton.onclick = function () {
     if (parseInt(firstNumber.value) < parseInt(secondNumber.value)) {
         excersThree.innerHTML = "-1";
         console.log("-1");
@@ -42,8 +30,21 @@ function excersizeThree(firstNumber, secondNumber) {
     }
 }
 
-excersThreeButton.addEventListener("click", excersizeThree(firstNumber, secondNumber));
+// _____4______
 
+let excersFourInputNumber = document.querySelector(".excersize-four__input");
+let excersFourButton = document.querySelector(".excersize-four__button");
+let excersFourUnswer = document.querySelector(".excersize-four__unswer");
 
+excersFourButton.onclick = function () {
+    function fact(num){
+        let mult = excersFourInputNumber;
+        for(i=1;i<=num;i++){
+            mult *= i; 
+        }
+        return mult;
+    }
+    excersFourUnswer.innerHTML = fact(excersFourInputNumber.value);
+}
 
 
