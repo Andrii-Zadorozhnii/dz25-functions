13c16efb9e93c157cf4755e1c22302d34dae0998
Andrii Zadorozhnii <zador746@gmail.com>
2023-03-04 02:32:48 +0200

    complete

--- calculation.js
+++ calculation.js
@@ -32,19 +32,71 @@ excersThreeButton.onclick = function () {
 
 // _____4______
 
-let excersFourInputNumber = document.querySelector(".excersize-four__input");
+
 let excersFourButton = document.querySelector(".excersize-four__button");
 let excersFourUnswer = document.querySelector(".excersize-four__unswer");
+let excersFourInputNumber = document.querySelector(".excersize-four__input");
+
+function factorial(n) {
+    if (n < 0) {
+        return -1;
+    } else if (n === 0) {
+        return 1;
+    } else if (n === undefined) {
+        console.log("undefined");
+    } else {
+        return (n * factorial(n - 1));
+    }
+}
 
 excersFourButton.onclick = function () {
-    function fact(num){
-        let mult = excersFourInputNumber;
-        for(i=1;i<=num;i++){
-            mult *= i; 
+    excersFourUnswer.innerHTML = factorial(parseInt(excersFourInputNumber.value));
+    console.log(factorial(parseInt(excersFourInputNumber.value)));
+}
+
+// _____5______
+
+let excersFiveButton = document.querySelector(".excersize-five__button");
+let listnumbers = [];
+
+excersFiveButton.onclick = function () {
+    for (let i = 0; i < 3; i++) {
+        let a = +prompt("Write a number");
+        listnumbers.push(a);
+        if (listnumbers.length === 3) {
+            let convertToString = String(listnumbers[0]) + String(listnumbers[1]) + String(listnumbers[2]);
+            let convertToNumber = parseInt(convertToString);
+            alert("typeof : " + typeof (convertToNumber) + " => " + convertToNumber);
+            console.log(typeof (convertToNumber));
+            console.log(String(convertToNumber));
         }
-        return mult;
     }
-    excersFourUnswer.innerHTML = fact(excersFourInputNumber.value);
 }
 
 
+// _____6______
+
+let heightSize = document.querySelector(".excersize-six_input-height");
+let widthSize = document.querySelector(".excersize-six_input-width");
+let excersSixButton = document.querySelector(".excersize-six__button");
+let excersSixUnswer = document.querySelector(".excersize-six__unswer");
+
+excersSixButton.onclick = function () {
+    console.log(typeof(heightSize.value));
+    console.log(typeof(widthSize.value));
+    if (parseInt(heightSize.value) > 0 && parseInt(widthSize.value) > 0) {
+        let square = parseInt(widthSize.value) * parseInt(heightSize.value);
+        excersSixUnswer.innerHTML = square;
+        console.log("1");
+    } else if ((parseInt(heightSize.value) === 0 || undefined || NaN || null) && parseInt(widthSize.value) > 0) {
+        let square = parseInt(widthSize.value) ** 2
+        excersSixUnswer.innerHTML = square;
+        console.log("2");
+    } else if (parseInt(heightSize.value) > 0 && (parseInt(widthSize.value) === 0)) {
+        let square = parseInt(heightSize.value) ** 2
+        excersSixUnswer.innerHTML = square;
+        console.log("3");
+    } else {
+        excersSixUnswer.innerHTML = "please enter valid input";
+    }
+}
